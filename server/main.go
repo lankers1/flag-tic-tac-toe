02342771c5d4f5819f03e670fffa96926bca2ba4
Lankers1 <james.ciaramella@gmail.com>
2024-09-19 21:48:30 +0100

    remove boilerplate

--- server/main.go
+++ server/main.go
@@ -6,13 +6,28 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 	"fmt"
+	"net/http"
+
 	"os"
 	"context"
-	"fttt/controllers"
-	"fttt/repositories"
-	"fttt/services"
 	"log"
 )
+
+
+type Game struct {
+	FirstRowId  int  `json:"first_row_id"`
+	FirstRow  string  `json:"first_row"`
+	FirstColumnId  int  `json:"first_column_id"`
+	FirstColumn  string  `json:"first_column"`
+	SecondRowId int  `json:"second_row_id"`
+	SecondRow  string `json:"second_row"`
+	SecondColumnId int  `json:"second_column_id"`
+	SecondColumn  string `json:"second_column"`
+	ThirdRowId int  `json:"third_row_id"`
+	ThirdRow  string `json:"third_row"`
+	ThirdColumnId int  `json:"third_column_id"`
+	ThirdColumn  string `json:"third_column"`
+}
 
 func goDotEnvVariable(key string) string {
   err := godotenv.Load(".env")
@@ -24,22 +39,51 @@ func goDotEnvVariable(key string) string {
   return os.Getenv(key)
 }
 
+func getGame(ctx *gin.Context,  conn *pgx.Conn) {
+	var game Game
+
+	queryErr := conn.QueryRow(context.Background(), "SELECT * FROM generate_game").Scan(
+		&game.FirstRowId,
+		&game.FirstRow,
+		&game.FirstColumnId,
+		&game.FirstColumn,
+		&game.SecondRowId,
+		&game.SecondRow,
+		&game.SecondColumnId,
+		&game.SecondColumn,
+		&game.ThirdRowId,
+		&game.ThirdRow,
+		&game.ThirdColumnId,
+		&game.ThirdColumn,
+		)
+
+
+		if queryErr != nil {
+			log.Fatalf("Error running query: %v", queryErr)
+		 }
+	
+
+	ctx.JSON(http.StatusOK, game)
+}
+
 func main() {
-	conn, err := pgx.Connect(context.Background(), goDotEnvVariable("DATABASE_URL"))
-	if err != nil {
-		log.Fatalf("Unable to connect to database: %v\n", err)
+	conn, error := pgx.Connect(context.Background(), goDotEnvVariable("DATABASE_URL"))
+	if error != nil {
+		log.Fatalf("Unable to connect to database: %v\n", error)
 	}
 
 	defer conn.Close(context.Background())
 
 	r := gin.Default()
-	gameRepo := repositories.InitGameRepository(conn)
-	gameService := services.InitGameService(gameRepo)
-	gameController := controllers.InitGameController(gameService)
+
+	
+
 
 	r.Use(cors.Default())
 
-	r.GET("/game", gameController.GetGame)
+	r.GET("/game", func(ctx *gin.Context) {
+		getGame(ctx, conn)
+  })
 
 	rErr := r.Run()
 
