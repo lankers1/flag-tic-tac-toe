efe33592a62c4ead62641743fffde95285e508be
Lankers1 <james.ciaramella@gmail.com>
2024-09-27 13:46:04 +0100

    initial answer flag modal

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,14 +1,15 @@
+import { PropsWithChildren } from "react";
 import styles from "./styles.module.scss";
 
 interface Props {
   isOpen: boolean;
 }
 
-export const Modal = ({ isOpen }: Props) => {
+export const Modal = ({ isOpen, children }: PropsWithChildren<Props>) => {
   if (!isOpen) return null;
   return (
     <div className={styles.modalBackground}>
-      <div className={styles.modal}></div>
+      <div className={styles.modal}>{children}</div>
     </div>
   );
 };
