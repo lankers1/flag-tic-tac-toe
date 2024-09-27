9af33bcef0e3e9125f301a86a5b58b8758dda6ad
Lankers1 <james.ciaramella@gmail.com>
2024-09-27 16:37:21 +0100

    IconButton component

---
+++ client/src/components/Buttons/IconButton.tsx
@@ -0,0 +1,27 @@
+import { IconType } from "react-icons";
+
+import styles from "./styles.module.scss";
+
+interface Props {
+  handleClick: () => void;
+  height?: string;
+  width?: string;
+  Icon: IconType;
+}
+
+export const IconButton = ({
+  handleClick,
+  height = "1.25rem",
+  width = "1.25rem",
+  Icon,
+}: Props) => (
+  <button onClick={handleClick} className={styles.button}>
+    <Icon
+      className={styles.icon}
+      style={{
+        height,
+        width,
+      }}
+    />
+  </button>
+);
