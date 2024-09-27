3fd6de8ead9dc8d416f58a874c27e4feb0a66e40
Lankers1 <james.ciaramella@gmail.com>
2024-09-27 16:56:43 +0100

    SearchFlags query hook

---
+++ client/src/query-hooks/searchFlags.ts
@@ -0,0 +1,12 @@
+import { useMutation } from "@tanstack/react-query";
+
+export const searchFlags = () =>
+  useMutation<Flags[], void, string>({
+    mutationFn: async (search_term: string) => {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/search_flags`, {
+        method: "POST",
+        body: JSON.stringify({ search_term }),
+      });
+      return await res.json();
+    },
+  });
