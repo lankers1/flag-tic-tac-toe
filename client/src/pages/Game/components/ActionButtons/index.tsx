22fc77d1380ce0b7358017a1dabe8b5607ecd89f
Lankers1 <james.ciaramella@gmail.com>
2024-10-14 16:10:23 +0100

    refactor

--- client/src/pages/Game/components/ActionButtons/index.tsx
+++ client/src/pages/Game/components/ActionButtons/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { GrPowerReset } from "react-icons/gr";
+
 import { LinkButton } from "../../../../components/Buttons/LinkButton";
 import { Button } from "../../../../components/Buttons/Button";
 import { Heading } from "../../../../components/Heading";
@@ -18,14 +19,13 @@ export const ActionButtons = ({
   handleResetGame,
   resetQuery,
 }: Props) => {
-  const [displayGiveUpModal, setDisplayGiveUpModal] = useState(false);
-  const [displayRestartGameModal, setDisplayRestartGameModal] = useState(false);
+  const [openModal, setOpenModal] = useState<string | boolean>(false);
 
   return (
     <div className={styles.container}>
-      <Button handleClick={() => setDisplayGiveUpModal(true)} label="Give up" />
+      <Button handleClick={() => setOpenModal("give_up")} label="Give up" />
       <Button
-        handleClick={() => setDisplayRestartGameModal(true)}
+        handleClick={() => setOpenModal("restart")}
         label={
           <>
             <GrPowerReset className={styles.buttonIcon} />
@@ -33,31 +33,61 @@ export const ActionButtons = ({
           </>
         }
       />
-      <Modal isOpen={displayGiveUpModal}>
-        <Heading variant="h3">Are you sure you want to give up?</Heading>
-        <div className={styles.buttonContainer}>
-          <Button handleClick={() => setDisplayGiveUpModal(false)} label="No" />
-          <LinkButton handleClick={resetQuery} to="/" label="Yes" />
-        </div>
-      </Modal>
-      <Modal isOpen={displayRestartGameModal}>
-        <Heading variant="h3">
-          Are you sure you want to restart the game?
-        </Heading>
-        <div className={styles.buttonContainer}>
-          <Button
-            handleClick={() => setDisplayRestartGameModal(false)}
-            label="No"
-          />
-          <Button
-            handleClick={() => {
-              setDisplayRestartGameModal(false);
-              handleResetGame();
-            }}
-            label="yes"
-          />
-        </div>
+      <Modal isOpen={!!openModal}>
+        <ModalContent
+          type={openModal}
+          closeModal={() => setOpenModal(false)}
+          handleResetGame={handleResetGame}
+          resetQuery={resetQuery}
+        />
       </Modal>
     </div>
   );
+};
+
+interface ModalContentProps {
+  type: "give_up" | "restart";
+  closeModal: () => void;
+  resetQuery: () => void;
+  handleResetGame: () => void;
+}
+
+const ModalContent = ({
+  type,
+  closeModal,
+  resetQuery,
+  handleResetGame,
+}: ModalContentProps) => {
+  switch (type) {
+    case "give_up":
+      return (
+        <>
+          <Heading variant="h3">Are you sure you want to give up?</Heading>
+          <div className={styles.buttonContainer}>
+            <Button handleClick={closeModal} label="No" />
+            <LinkButton handleClick={resetQuery} to="/" label="Yes" />
+          </div>
+        </>
+      );
+    case "restart":
+      return (
+        <>
+          <Heading variant="h3">
+            Are you sure you want to restart the game?
+          </Heading>
+          <div className={styles.buttonContainer}>
+            <Button handleClick={closeModal} label="No" />
+            <Button
+              handleClick={() => {
+                closeModal();
+                handleResetGame();
+              }}
+              label="Yes"
+            />
+          </div>
+        </>
+      );
+    default:
+      return;
+  }
 };
