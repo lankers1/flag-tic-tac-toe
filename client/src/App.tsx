5d6f7ff74b54b3188fc22c49768a6d8b9d3f0c68
Lankers1 <james.ciaramella@gmail.com>
2024-10-28 15:12:26 +0000

    test tsc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
