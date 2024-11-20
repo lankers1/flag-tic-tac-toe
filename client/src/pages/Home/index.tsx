b9c6afa174410e305be6bc574877571b760d0c9f
Lankers1 <james.ciaramella@gmail.com>
2024-11-20 16:14:33 +0000

    new version

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,10 +1,10 @@
-import { FaRobot, FaUserFriends } from "react-icons/fa";
+import { FaRobot, FaUserFriends } from 'react-icons/fa';
 
-import { LinkButton } from "../../components/Buttons/LinkButton";
-import { Card } from "../../components/Card";
-import { Heading } from "../../components/Heading";
+import { LinkButton } from '../../components/Buttons/LinkButton';
+import { Card } from '../../components/Card';
+import { Heading } from '../../components/Heading';
 
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
 export const Home = () => {
   return (
@@ -14,8 +14,8 @@ export const Home = () => {
           <div className={styles.subheading}>
             <Heading variant="h2">Play!</Heading>
             <Heading variant="h3">
-              Aim to beat your opponent by guessing the flags based on
-              categories. The first to complete three in a row, column or
+              NEW VERSION Aim to beat your opponent by guessing the flags based
+              on categories. The first to complete three in a row, column or
               diagonally wins.
             </Heading>
           </div>
@@ -26,7 +26,7 @@ export const Home = () => {
               label={
                 <>
                   <FaUserFriends className={styles.buttonIcons} />
-                  {"Local Play"}
+                  {'Local Play'}
                 </>
               }
             />
@@ -36,7 +36,7 @@ export const Home = () => {
               label={
                 <>
                   <FaRobot className={styles.buttonIcons} />
-                  {"Vs Computer"}
+                  {'Vs Computer'}
                 </>
               }
             />
