d9932744e28b7b8fcd437da882307a0b72f41317
Lankers1 <james.ciaramella@gmail.com>
2025-01-09 08:52:54 +0000

    MT-1133 path aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 8000,
+    port: 8000
   },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@computer': path.resolve(__dirname, './src/computer'),
+      '@query-hooks': path.resolve(__dirname, './src/query-hooks'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils')
+    }
+  }
 });
