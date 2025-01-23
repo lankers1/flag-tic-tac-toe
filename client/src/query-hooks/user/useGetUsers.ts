c4a08600e98dc46e33ee524bd5d57d1f9aabc2f5
Lankers1 <james.ciaramella@gmail.com>
2025-01-23 20:51:41 +0000

    useGetUsers hook

---
+++ client/src/query-hooks/user/useGetUsers.ts
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+
+async function handleGetUsers() {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/users`);
+  return await res.json();
+}
+
+export const useGetUsersQuery = () => {
+  return useQuery({
+    queryKey: ['users'],
+    gcTime: 0,
+    queryFn: handleGetUsers,
+    refetchOnWindowFocus: false
+  });
+};
