36e126248aa41117b8e607780d6c1589f8a7cdde
Lankers1 <james.ciaramella@gmail.com>
2025-01-24 23:04:01 +0000

    IconButton disabled styling

--- client/src/components/common/Buttons/IconButton.tsx
+++ client/src/components/common/Buttons/IconButton.tsx
@@ -1,26 +1,32 @@
-import { IconType } from "react-icons";
+import { IconType } from 'react-icons';
 
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
 interface Props {
   handleClick: () => void;
   height?: string;
   width?: string;
   Icon: IconType;
+  disabled?: boolean;
 }
 
 export const IconButton = ({
   handleClick,
-  height = "1.25rem",
-  width = "1.25rem",
+  height = '1.25rem',
+  width = '1.25rem',
   Icon,
+  disabled
 }: Props) => (
-  <button onClick={handleClick} className={styles.iconButton}>
+  <button
+    onClick={handleClick}
+    disabled={disabled}
+    className={styles.iconButton}
+  >
     <Icon
       className={styles.icon}
       style={{
         height,
-        width,
+        width
       }}
     />
   </button>
