97e1a6657612a67450a510e36698d675ed4f296b
Lankers1 <james.ciaramella@gmail.com>
2025-03-12 14:08:01 +0000

    update handleUpdateUserRank to call /user/:username/score endpoint

--- client/src/query-hooks/user/useUpdateUserRank.ts
+++ client/src/query-hooks/user/useUpdateUserRank.ts
@@ -7,10 +7,13 @@ interface Args {
 }
 
 async function handleUpdateUserRank({ username, token, result }: Args) {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/user/${username}`, {
-    method: 'PATCH',
-    body: JSON.stringify({ token, result })
-  });
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/user/${username}/score`,
+    {
+      method: 'PATCH',
+      body: JSON.stringify({ token, result })
+    }
+  );
 
   if (!res.ok) {
     const error = await res.json();
