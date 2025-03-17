74b39a5351a934337a9950745ac437b43a0b5405
Lankers1 <james.ciaramella@gmail.com>
2025-03-17 14:54:56 +0000

    Retrieve account data from useGetUserQuery

--- client/src/pages/Account/index.tsx
+++ client/src/pages/Account/index.tsx
@@ -14,10 +14,14 @@ import { LinkButton } from '@components/common/Buttons/LinkButton';
 import { FaChevronLeft } from 'react-icons/fa';
 import { Modal } from '@components/common/Modal';
 import { SearchFlagModalContent } from '@components/Account/SearchFlagModalContent';
+import { useGetUserQuery } from '@query-hooks/user/useGetUser';
+import { Loader } from '@components/common/Loader';
+import { Notification } from '@components/common/Notification';
 
 export const Account = () => {
   const [displayFlagModal, setDisplayFlagModal] = useState(false);
-  const user = useContext(AuthContext) as UserContext;
+  const userContext = useContext(AuthContext) as UserContext;
+  const { data, isLoading, isError } = useGetUserQuery(userContext?.username);
 
   function closeFlagModal() {
     setDisplayFlagModal(false);
@@ -27,40 +31,50 @@ export const Account = () => {
     <>
       <Card className={styles.card}>
         <FlexDiv flexDirection="col" className={styles.cardContainer}>
-          <Heading variant="h3">Account details</Heading>
-          <TextInput
-            name="username"
-            label="Username"
-            disabled
-            placeholder="Username"
-            value={user?.username}
-          />
-          <FlexDiv alignItems="flexStart" flexDirection="col">
-            <Text>Favourite flag</Text>
-            <Button
-              handleClick={() => setDisplayFlagModal(true)}
-              label={
-                <>
-                  <FlagAvatar
-                    className={styles.flagAvatar}
-                    flagIso2={user?.favouriteFlag}
-                  />
-                  Change flag
-                </>
-              }
-            />
-          </FlexDiv>
-          <FlexDiv justifyContent="spaceBetween">
-            <LinkButton
-              to="/"
-              label={
-                <>
-                  <FaChevronLeft />
-                  Back
-                </>
-              }
-            />
-          </FlexDiv>
+          {isError && (
+            <Notification type="error">
+              Something went wrong while retireving your account
+            </Notification>
+          )}
+          {isLoading && <Loader />}
+          {data?.user && (
+            <>
+              <Heading variant="h3">Account details</Heading>
+              <TextInput
+                name="username"
+                label="Username"
+                disabled
+                placeholder="Username"
+                value={data?.user?.username}
+              />
+              <FlexDiv alignItems="flexStart" flexDirection="col">
+                <Text>Favourite flag</Text>
+                <Button
+                  handleClick={() => setDisplayFlagModal(true)}
+                  label={
+                    <>
+                      <FlagAvatar
+                        className={styles.flagAvatar}
+                        flagIso2={data?.user?.favouriteFlag}
+                      />
+                      Change flag
+                    </>
+                  }
+                />
+              </FlexDiv>
+              <FlexDiv justifyContent="spaceBetween">
+                <LinkButton
+                  to="/"
+                  label={
+                    <>
+                      <FaChevronLeft />
+                      Back
+                    </>
+                  }
+                />
+              </FlexDiv>
+            </>
+          )}
         </FlexDiv>
       </Card>
       <Modal isOpen={displayFlagModal}>
