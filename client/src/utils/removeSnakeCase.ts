2583a08936879013cc0b803694c109450fcd7668
Lankers1 <james.ciaramella@gmail.com>
2025-03-24 19:13:53 +0000

    capitalise helper function

--- client/src/utils/removeSnakeCase.ts
+++ client/src/utils/removeSnakeCase.ts
@@ -1,1 +1,3 @@
-export const removeSnakeCase = (str: string) => str.split("_").join(" ");
+export const removeSnakeCase = (str: string) => str.split('_').join(' ');
+export const capitalise = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1);
