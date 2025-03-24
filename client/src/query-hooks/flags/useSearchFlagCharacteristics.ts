aa5c6c3e15c47234bb487bdfe681ba6c1cda661c
Lankers1 <james.ciaramella@gmail.com>
2025-03-24 19:14:13 +0000

    useSearchFlagCharacteristicsQuery hook

---
+++ client/src/query-hooks/flags/useSearchFlagCharacteristics.ts
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+
+async function handleSearchFlagCharacteristics(
+  searchTerm: string,
+  characteristics: string[]
+) {
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/search_flag_characteristics`,
+    {
+      method: 'POST',
+      body: JSON.stringify({ search_term: searchTerm, characteristics })
+    }
+  );
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw Error(error);
+  }
+
+  return await res.json();
+}
+
+export const useSearchFlagCharacteristicsQuery = (
+  searchTerm: string,
+  characteristics: string[]
+) =>
+  useQuery<Flag[]>({
+    queryKey: [searchTerm, ...characteristics],
+    queryFn: () => handleSearchFlagCharacteristics(searchTerm, characteristics)
+  });
